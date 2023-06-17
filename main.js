24649e6cbb46452680b1f1499f3154b74331cd84
VavasseurKevin <minhluan.phan@gmail.com>
2023-06-17 13:18:56 +0200

    use Scroll Reveal librairy to creata Effet display

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 // Navbar
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
- 
+
 menu.onclick = () => {
     navbar.classList.toggle('active');
 }
- 
+
 window.onscroll = () => {
     navbar.classList.remove('active');
 }
+
+// Scroll Reveal
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '40px',
+    duration: 2000,
+    reset: true
+});
+
+
+sr.reveal(`.accueil-text, .accueil-img,
+                .nous-img, .nous-text,
+                .box-menu, .services-box,
+                .btn, .connect-text,
+                .contact-box`, {
+    interval: 200
+})
