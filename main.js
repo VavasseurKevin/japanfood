4e99539ce4398280638fff7a4fd54fdcedf90cd2
VavasseurKevin <minhluan.phan@gmail.com>
2023-06-17 13:24:35 +0200

    Add Dark-mode using JS

--- main.js
+++ main.js
@@ -26,3 +26,16 @@ sr.reveal(`.accueil-text, .accueil-img,
                 .contact-box`, {
     interval: 200
 })
+
+// Dark Mode
+let darkmode = document.querySelector('#darkmode');
+
+darkmode.onclick = () => {
+    if (darkmode.classList.contains('bx-moon')) {
+        darkmode.classList.replace('bx-moon', 'bx-sun');
+        document.body.classList.add('active');
+    } else {
+        darkmode.classList.replace('bx-sun', 'bx-moon');
+        document.body.classList.remove('active');
+    }
+}
